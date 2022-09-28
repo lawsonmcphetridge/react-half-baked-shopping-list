c80ae2e43bb014710ff51d06dd52b92850aca15d
Lawson McPhetridge <106125303+lawsonmcphetridge@users.noreply.github.com>
2022-09-28 14:30:01 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Auth from './components/Auth/Auth';
 import Header from './components/Header';
 import Items from './components/Items/Items';
-import { UserContext } from './context/UserProvider';
+import { UserContext } from './context/UserContext';
 import backgroundImage from './supermarket.jpeg';
 
 function App() {
